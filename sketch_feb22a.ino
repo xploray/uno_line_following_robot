b0178272d4611a95a078dd5f91147c0b954e9de6
saikksub <saikksub@gmail.com>
2026-02-28 20:44:55 +0530

    update

--- sketch_feb22a.ino
+++ sketch_feb22a.ino
@@ -9,8 +9,7 @@
 #define IN3 7
 #define IN4 8
 
-int baseSpeed = 170;
-int turnDelay = 700;
+int baseSpeed = 80;
 
 void setup() {
   pinMode(IR_LEFT, INPUT);
@@ -37,37 +36,30 @@ void forward() {
 }
 
 void left() {
-  // Left motor backward
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
 
-  // Right motor forward
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 
   analogWrite(ENA, baseSpeed);
   analogWrite(ENB, baseSpeed);
-    delay(turnDelay);
 }
 
 void right() {
-  // Left motor forward
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
 
-  // Right motor backward
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 
   analogWrite(ENA, baseSpeed);
   analogWrite(ENB, baseSpeed);
-    delay(turnDelay);
 }
 
 void stopBot() {
   analogWrite(ENA, 0);
   analogWrite(ENB, 0);
-  delay(1000);
 }
 
 void loop() {
@@ -75,17 +67,19 @@ void loop() {
   int leftSensor = !digitalRead(IR_LEFT);
   int rightSensor = !digitalRead(IR_RIGHT);
 
+  // If both detect line
   if (leftSensor == HIGH && rightSensor == HIGH) {
     forward();
   }
-  else if (leftSensor == HIGH && rightSensor == LOW) {
-    stopBot();
-    left();
-  }
+  // If left detects line → turn left
   else if (leftSensor == LOW && rightSensor == HIGH) {
-    stopBot();
     right();
   }
+  // If right detects line → turn right
+  else if (leftSensor == HIGH && rightSensor == LOW) {
+    left();
+  }
+  // If no line detected
   else {
     stopBot();
   }
